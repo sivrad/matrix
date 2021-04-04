9bad7d17a5cf6f89cb08b7d13b321df26266a1d2
Max Koon <theakman96@gmail.com>
2021-04-03 23:15:01 -0400

    exported SerializedMatrixBaseType

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { MatrixBaseType } from './matrixBaseType';
+export { MatrixBaseType, SerializedMatrixBaseType } from './matrixBaseType';
 export { Matrix } from './matrix';
 export { Collection } from './collection';
 export { Field } from './type';
