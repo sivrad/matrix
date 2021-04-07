86d9d871a7e74e837bd870fe76c8dfc0196e6039
Max Koon <theakman96@gmail.com>
2021-04-06 21:26:00 -0400

    v1.0.5

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { Matrix } from './matrix';
 export { Collection } from './collection';
 export { Field } from './type';
 export { ReadonlySource, Source } from './source';
+export * as errors from './errors';
