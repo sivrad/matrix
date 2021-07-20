efa818e3adeec5da86f0f4675b34772480ab1cd7
Max Koon <theakman96@gmail.com>
2021-07-20 10:33:22 -0400

    rework

--- src/core/decorators.ts
+++ src/core/decorators.ts
@@ -1,36 +1,41 @@
 import { AlreadyInstantiated, Uninstantiated } from './error';
 import { MatrixBaseType } from './matrixBaseType';
 
-const makeDecorator = (shouldBeInstance = true) => () => {
+/**
+ * Force a method to be called by an instance.
+ * @returns {Function} The decorator.
+ */
+export const instanceMethod = () => {
     return (
         target: MatrixBaseType,
         _: string,
         descriptor: PropertyDescriptor,
     ): PropertyDescriptor => {
         const originalMethod = descriptor.value;
-
         descriptor.value = function (...args: unknown[]) {
-            // Throw an error if instance state does not match `shouldBeInstance`.
-            if (shouldBeInstance && !target.isInstance())
+            if (!target.isInstance())
                 throw new Uninstantiated(target.getTypeClass());
-            else if (!shouldBeInstance && target.isInstance())
-                throw new AlreadyInstantiated(target);
-
             return originalMethod.apply(this, args);
         };
-
         return descriptor;
     };
 };
 
 /**
- * Force a method to be called by an instance.
- * @returns {Function} The decorator.
- */
-export const instanceMethod = makeDecorator(true);
-
-/**
  * Force a method to be called by a type.
  * @returns {Function} The decorator.
  */
-export const typeMethod = makeDecorator(false);
+export const typeMethod = () => {
+    return (
+        target: MatrixBaseType,
+        _: string,
+        descriptor: PropertyDescriptor,
+    ): PropertyDescriptor => {
+        const originalMethod = descriptor.value;
+        descriptor.value = function (...args: unknown[]) {
+            if (target.isInstance()) throw new AlreadyInstantiated(target);
+            return originalMethod.apply(this, args);
+        };
+        return descriptor;
+    };
+};
